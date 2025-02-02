087bcab88f29ebbff04f012e6c4734597baadc0e
Veha <weha.kriadchaingam@gmail.com>
2025-02-02 22:03:33 +0700

    bug fixed

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull dev/weha
 yarn install # หรือใช้ npm install / yarn install
 
 # 3. Build Nuxt.js
-yarn build # หรือ npm run build / yarn build
+NODE_OPTIONS="--max-old-space-size=512" yarn build # หรือ npm run build / yarn build
 
 # 4. Reload PM2
 pm2 reload ecosystem.config.js --env production
