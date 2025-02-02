cef6cc8b800cfe348ce09ba640208a0a2c1b155d
Veha <weha.kriadchaingam@gmail.com>
2025-02-02 22:14:45 +0700

    bug fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ export default {
       port: 3000,
       exec_mode: "cluster",
       instances: "max",
-      script: "./.output/server/index.mjs", // ต้องมีไฟล์นี้
+      // script: "./.output/server/index.mjs", // ต้องมีไฟล์นี้
+      script: "/home/slowboat-laos/htdocs/slowboat-laos.com/.output/server/index.mjs",
       env: {
         NODE_ENV: "production"
       }
