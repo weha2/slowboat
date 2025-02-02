5ffba976b096d049e900c692859fb1466db27699
Veha <weha.kriadchaingam@gmail.com>
2025-02-02 22:22:44 +0700

    bug fixed

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#chmod +x deploy.sh
+# run : chmod +x deploy.sh
+# run : ./deploy.sh
 
 # 1. ดึงโค้ดล่าสุด (ถ้าใช้ Git)
 git pull
@@ -8,7 +9,7 @@ git pull
 yarn install # หรือใช้ npm install / yarn install
 
 # 3. Build Nuxt.js
-NODE_OPTIONS="--max-old-space-size=512" pnpm run build  # หรือ npm run build / yarn build
+NODE_OPTIONS="--max-old-space-size=1024" pnpm run build  # หรือ npm run build / yarn build
 
 # 4. Reload PM2
 pm2 reload ecosystem.config.js --env production
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,10 @@ export default {
   apps: [
     {
       name: "slowboat",
-      port: 3000,
       exec_mode: "cluster",
       instances: "max",
-      script: "./.output/server/index.mjs", // ต้องมีไฟล์นี้
-      env: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start",
+    },
+  ],
 };
